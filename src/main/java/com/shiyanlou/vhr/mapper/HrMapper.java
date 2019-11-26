caaaf5cf685da672641ca5ef5f3ada1865793c86
SinggleXGC <1307708161@qq.com>
2019-11-26 13:14:19 +0800

    实验11 微人事系统之实现操作员管理

--- src/main/java/com/shiyanlou/vhr/mapper/HrMapper.java
+++ src/main/java/com/shiyanlou/vhr/mapper/HrMapper.java
@@ -2,19 +2,30 @@ package com.shiyanlou.vhr.mapper;
 
 import com.shiyanlou.vhr.bean.Hr;
 import com.shiyanlou.vhr.bean.Role;
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@Mapper
 public interface HrMapper {
-
-    //通过名字查找
-    Hr loadUserByUsername(@Param("username") String username);
-
-    //通过Hr的id查找角色
+    //获取对应的用户
+    Hr loadUserByUsername(String username);
+    //获取对应的角色
     List<Role> getRolesByHrId(Long id);
+    //获取hr
+    Hr getHrById(Long hrId);
+    //删除hr
+    int deleteHr(Long hrId);
+    //修改hr
+    int updateHr(Hr hr);
+    //删除hr的角色
+    int deleteRoleByHrId(Long hrId);
+    //添加hr的角色
+    int addRolesForHr(@Param("hrId") Long hrId, @Param("rids") Long[] rids);
+    //通过关键字查找hr
+    List<Hr> getHrsByKeywords(@Param("keywords") String keywords);
+
+    int hrReg(@Param("username") String username, @Param("password") String password);
+
 }
--- src/main/java/com/shiyanlou/vhr/service/HrService.java
+++ src/main/java/com/shiyanlou/vhr/service/HrService.java
@@ -1,14 +1,16 @@
 package com.shiyanlou.vhr.service;
 
-import com.shiyanlou.vhr.bean.Hr;
 import com.shiyanlou.vhr.mapper.HrMapper;
+import com.shiyanlou.vhr.bean.Hr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 
 @Service
 @Transactional
@@ -19,9 +21,42 @@ public class HrService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
         Hr hr = hrMapper.loadUserByUsername(s);
-        if (hr == null)
+        if(hr==null)
             throw new UsernameNotFoundException("用户名不对");
         return hr;
     }
+
+    public int hrReg(String username, String password) {
+        //如果用户名存在，返回错误
+        if (hrMapper.loadUserByUsername(username) != null) {
+            return -1;
+        }
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encode = encoder.encode(password);
+        return hrMapper.hrReg(username, encode);
+    }
+
+    public List<Hr> getHrsByKeywords(String keywords) {
+        return hrMapper.getHrsByKeywords(keywords);
+    }
+
+    public int updateHr(Hr hr) {
+        return hrMapper.updateHr(hr);
+    }
+
+    public int updateHrRoles(Long hrId, Long[] rids) {
+        int i = hrMapper.deleteRoleByHrId(hrId);
+        return hrMapper.addRolesForHr(hrId, rids);
+    }
+
+    public Hr getHrById(Long hrId) {
+        return hrMapper.getHrById(hrId);
+    }
+
+    public int deleteHr(Long hrId) {
+        return hrMapper.deleteHr(hrId);
+    }
+
 }
